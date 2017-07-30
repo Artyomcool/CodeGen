7b9199c2d79e230d5495a3e9e57df177b070f8ab
Artyom Drozdov <artyomcool2@gmail.com>
2017-07-30 11:26:53 +0300

    Flurry integration

---
+++ app/src/main/java/ru/mail/android/meetup/codegen/MyApplication.java
@@ -0,0 +1,14 @@
+package ru.mail.android.meetup.codegen;
+
+import android.app.Application;
+
+import com.flurry.android.FlurryAgent;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FlurryAgent.init(this, "7VVG4RYZV4J9B9J2Z4VP");
+    }
+}
