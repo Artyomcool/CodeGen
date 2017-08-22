8e8db174da8f03671b05ad0cd2c6bcb60f943ee1
Artyom Drozdov <artyomcool2@gmail.com>
2017-08-22 10:42:16 +0300

    Introspection

--- app/src/main/java/ru/mail/android/meetup/codegen/Stat.java
+++ app/src/main/java/ru/mail/android/meetup/codegen/Stat.java
@@ -1,5 +1,7 @@
 package ru.mail.android.meetup.codegen;
 
+import android.util.Log;
+
 import com.flurry.android.FlurryAgent;
 import com.google.android.gms.analytics.HitBuilders;
 
@@ -21,14 +23,26 @@ public class Stat {
         ga(category, action);
     }
 
+    private static void all() {
+        Throwable throwable = new Throwable();
+        int depth = 2;      //because of synthetic accessor
+        StackTraceElement element = throwable.getStackTrace()[depth];
+
+        String className = element.getClassName();
+        String methodName = element.getMethodName();
+
+        className = className.substring(className.lastIndexOf("$") + 1);
+        all(className, methodName);
+    }
+
     public static class Main {
 
         public static void fabClicked() {
-            all("Main", "fabClicked");
+            all();
         }
 
         public static void settingsClicked() {
-            all("Main", "settingsClicked");
+            all();
         }
 
     }
